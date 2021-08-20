9a1205cdedb269b3c2b1a9786866206af786ffd4
sjoongh <az45687@naver.com>
2021-08-20 15:03:05 +0900

    1

---
+++ test.py
@@ -0,0 +1,13 @@
+T = int(input())
+M, N, K = map(int(input().split()))
+map = [0][0]
+
+for i in range(T):
+    map = int(input())
+
+for j in range(T):
+        T = 0 
+
+print(M)
+print(N)
+print(K)
